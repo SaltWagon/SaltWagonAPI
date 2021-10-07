93a30ee2095365997dbde0200b148388d2948593
soulpower11 <5299979+soulpower11@users.noreply.github.com>
2021-10-07 15:56:23 +0800

    Fixed CORS Error

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(function (err, req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization');
-//     if (req.method === 'OPTIONS') {
-//         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-//         return res.status(200).json({});
-//     }
-// })
 app.use(cors())
 
 app.use(function (err, req, res, next) {
