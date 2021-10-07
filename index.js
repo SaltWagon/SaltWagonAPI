4dc2b30c47a5d3c16dda293bbf8ecf95cc6ddef4
soulpower11 <5299979+soulpower11@users.noreply.github.com>
2021-10-07 15:36:51 +0800

    Fixing Cors error

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ const PORT = process.env.PORT || 3000;
 
 app.use('/api', apiRouter);
 
-app.use(function(err,req,res,next){
-    res.status(422).send({error: err.message});
+app.use(function (err, req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+})
+
+app.use(function (err, req, res, next) {
+    res.status(422).send({ error: err.message });
 });
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log('Server Started');
 })
